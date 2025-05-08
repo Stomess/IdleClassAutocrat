139d7b62c8063765352a95be7a78e278587c7507
Stomess <nexcalibar@gmx.net>
2025-05-08 15:44:17 +0200

    updated click-earning feedback
    turns out, with all that "secret" background-clicking, the actual html-text is never updated ( therefore show always 1 ) .. fixed that real "quick"

--- AutocratV3.js
+++ AutocratV3.js
@@ -92,7 +92,7 @@ class IdleClassAutocrat {
 		
 		this.autoEarnDollars = function() {
 			game.addManualClicks();
-			$(".top-click-value-display").fadeToggle()
+			$(".top-click-value-display").text(game.earnedPerClick.displayVal()).fadeToggle()
 		};
 		this.autoUpgrade = function() {
 			if(this.upgradeSpendFraction >= 1.0) {
